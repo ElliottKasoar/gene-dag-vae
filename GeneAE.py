3445bb4f1418494f481f958717518dae4a2812ba
Elliott Kasoar <elliott.kasoar@btinternet.com>
2020-08-11 21:45:02 +0100

    Initial AE implementation

--- GeneAE.py
+++ GeneAE.py
@@ -1,36 +1,41 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D
+from load import save_h5ad, load_h5ad
+from keras.layers import Input, Dense
 from keras.models import Model
-from keras.datasets import mnist
 from keras import regularizers
 import numpy as np
 import matplotlib.pyplot as plt
-
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import QuantileTransformer
 
 # =============================================================================
 # Model parameters
 # =============================================================================
 
 # Size of encoded representation
-encoding_dim = 32
+encoding_dim = 256
 
-# Input shape
-input_shape = (784,)
+train_size = 0.7
 
 # =============================================================================
 # Load data
 # =============================================================================
 
-(x_train, _), (x_test, _) = mnist.load_data()
+adata = load_h5ad('preprocessed')
 
-x_train = x_train.astype('float32') / 255.
-x_test = x_test.astype('float32') / 255.
-x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
-x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
-# print(x_train.shape)
-# print (x_test.shape)
+x = adata.X
+
+# Input shape
+input_dim = x.shape[1]
+input_shape = (input_dim,)
+
+qt = QuantileTransformer(n_quantiles=1000, output_distribution='normal')
+x = qt.fit_transform(x)
+scale = x.max(axis=0)
+x = np.divide(x, scale)
+x_train, x_test = train_test_split(x, train_size=train_size)
 
 # =============================================================================
 # Build models
@@ -43,15 +48,14 @@ AE_input = Input(shape=input_shape)
 # encoded = Dense(encoding_dim, activation='relu', 
 #                 activity_regularizer=regularizers.l1(10e-5))(VAE_input)
 
-encoded = Dense(128, activation='relu',
-                activity_regularizer=regularizers.l1(10e-5))(AE_input)
-encoded = Dense(64, activation='relu')(encoded)
+encoded = Dense(1024, activation='relu')(AE_input)
+encoded = Dense(512, activation='relu')(encoded)
 encoded = Dense(encoding_dim, activation='relu')(encoded)
 
 # Lossy reconstruction of the input
-decoded = Dense(64, activation='relu')(encoded)
+decoded = Dense(512, activation='relu')(encoded)
 decoded = Dense(128, activation='relu')(decoded)
-decoded = Dense(784, activation='sigmoid')(decoded)
+decoded = Dense(input_dim, activation='tanh')(decoded)
 
 # Model maps an input to its reconstruction
 autoencoder = Model(AE_input, decoded)
@@ -70,42 +74,21 @@ decoder = autoencoder.layers[-1](decoder)
 # Create decoder model
 decoder_model = Model(encoded_input, decoder)
 
-autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
 
 # =============================================================================
 # Train model
 # =============================================================================
 
-autoencoder.fit(x_train, x_train,
-                epochs=100,
-                batch_size=256,
-                shuffle=True,
-                validation_data=(x_test, x_test))
+autoencoder.fit(x_train, x_train, epochs=100, batch_size=256, shuffle=True)
+
+autoencoder.save('AE.h5')
 
 # =============================================================================
 # Test model
 # =============================================================================
 
-encoded_imgs = encoder_model.predict(x_test)
-decoded_imgs = decoder_model.predict(encoded_imgs)
-
-n = 10  # how many digits we will display
-plt.figure(figsize=(20, 4))
-for i in range(n):
-    # display original
-    ax = plt.subplot(2, n, i + 1)
-    plt.imshow(x_test[i].reshape(28, 28))
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-
-    # display reconstruction
-    ax = plt.subplot(2, n, i + 1 + n)
-    plt.imshow(decoded_imgs[i].reshape(28, 28))
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-plt.show()
-
+encoded_data = encoder_model.predict(x_test)
+decoded_data = decoder_model.predict(encoded_data)
 
 
